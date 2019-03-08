cc1a942998b45e7b52e460eaebe9d0d8a78e0335
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-03-08 13:59:31 -0700

    formatting home page

---
+++ client/src/components/HomeHeader/HomeHeader.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {  MDBRow, MDBCol, MDBCard, MDBContainer } from "mdbreact";
+
+const HomeHeader = () => {
+  return (
+   <> 
+    <MDBRow className="mb-1">
+        <MDBCol md="12" className="mb-1">
+          <MDBCard className="bg p-5 card-img-top" height="100%" style={{
+                  backgroundImage:
+                    "url(https://cdn.pixabay.com/photo/2018/05/04/10/30/team-3373638_1280.jpg)"
+                }}>
+            <div className="text-white text-center d-flex rgba-black-strong py-5 px-5">
+              <div>
+                <h1 className="py-3 font-weight-bold">
+                  <strong>Welcome!</strong>
+                </h1>
+                <h2 className="py-3 px-2">
+                  As developers we all want to create web applications that are intuitive and provide value to our users. Let's work together to make sure EVERYONE can use our sites. 
+                </h2>
+              </div>
+            </div>
+          </MDBCard>
+        </MDBCol>
+    </MDBRow>
+  </>
+  )
+}
+
+export default HomeHeader;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,26 @@
 import React from "react";
-import Header from "../components/Header/Header";
+import HomeHeader from "../components/HomeHeader/HomeHeader";
+import { MDBCol, MDBRow, MDBContainer } from "mdbreact";
 
 function Landing() {
     return (
         <>
-        {/* need to make the header reusable code, will get back to this! */}
-        <Header/>
-
+        <MDBContainer fluid>
+            <HomeHeader/>
+            <MDBRow>
+                <MDBCol md="2"></MDBCol>
+                <MDBCol md="8">
+                    <h3>Our Goal</h3>
+                </MDBCol>
+            </MDBRow>
+            <MDBRow>
+                <MDBCol md="2"></MDBCol>
+                <MDBCol md="8">
+                    <p className="px-4 py-1">To ensure web applications enable everyone to use them effectively and without frustration, web accessibility standards must be considered. That's where we come in! Our learning site provides resources and learning for developers to ensure their sites can be used by all. </p>
+                    <p className="px-4 py-1">As you work through our learning portal, join the conversation to ask questions, provide ideas and add answers for other developers working to improve their web accessibility standards. We look forward to engaging with you!</p>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
         </>
     );
 }
