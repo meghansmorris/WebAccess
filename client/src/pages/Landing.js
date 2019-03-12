487e4aa079d99923dea91c9972846fdf4e063776
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-03-11 21:30:38 -0600

    adding component for comment and updating landing page design

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -17,20 +17,20 @@ function Landing() {
             <MDBRow>
                 <MDBCol md="2"></MDBCol>
                 <MDBCol md="8">
-                    <p className="px-4 py-1">To ensure web applications enable everyone to use them effectively and without frustration, web accessibility standards must be considered. That's where we come in! Our learning site provides resources and learning for developers to ensure their sites can be used by all. </p>
-                    <p className="px-4 py-1">As you work through our learning portal, join the conversation to ask questions, provide ideas and add answers for other developers working to improve their web accessibility standards. We look forward to engaging with you!</p>
+                    <p className="justify-content-start px-4 py-1">To ensure web applications enable everyone to use them effectively and without frustration, web accessibility standards must be considered. That's where we come in! Our learning site provides resources and learning for developers to ensure their sites can be used by all. </p>
+                    <p className="justify-content-start px-4 py-1">As you work through our learning portal, join the conversation to ask questions, provide ideas and add answers for other developers working to improve their web accessibility standards. We look forward to engaging with you!</p>
                 </MDBCol>
             </MDBRow>
             <MDBRow>
                 <MDBCol md="2"></MDBCol>
-                <MDBCol md="3">
+                <MDBCol className=" ml-2 pl-4" md="3">
                     <MDBTooltip 
                             placement="bottom"
                             component="MDBCol"
-                            tag="a"
+                            tag="div"
                             tooltipContent="Join the Conversation">
                             <a href="/community" className="pl-1 mr-5">
-                                <MDBIcon far icon="comments" size="4x" className="grey-text pr-3" alt="join the conversation" />
+                                <MDBIcon icon="comments" size="4x" className="grey-text pr-3" alt="join the conversation" />
                             </a>                    
                         </MDBTooltip> 
                 </MDBCol>
@@ -38,10 +38,10 @@ function Landing() {
                     <MDBTooltip 
                         placement="bottom"
                         component="MDBCol"
-                        tag="a"
+                        tag="div"
                         tooltipContent="Learn Web Accessibility">
                         <a href="/learn" className="mr-5">
-                            <MDBIcon fas icon="puzzle-piece" size="4x" className="grey-text pr-3" alt="learn" />
+                            <MDBIcon icon="puzzle-piece" size="4x" className="grey-text pr-3" alt="learn" />
                         </a>                  
                     </MDBTooltip> 
                 </MDBCol>
@@ -49,13 +49,12 @@ function Landing() {
                     <MDBTooltip 
                         placement="bottom"
                         component="MDBCol"
-                        tag="a"
+                        tag="div"
                         tooltipContent="Take a Quiz">
                         <a href="/quiz" className="mr-4">
-                            <MDBIcon fas icon="question" size="4x" className="grey-text pr-3" alt="take a quiz" />
+                            <MDBIcon icon="question" size="4x" className="grey-text pr-3" alt="take a quiz" />
                         </a>                 
                     </MDBTooltip> 
-
                 </MDBCol>
             </MDBRow>
             <MDBRow>
