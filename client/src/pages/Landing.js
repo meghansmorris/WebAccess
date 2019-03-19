c957ffd7dffc0bfcac0fa2bca59e3062c170b6e1
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-03-19 15:12:49 -0600

    updating styling on resources, carousel buttons and landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,62 +1,133 @@
 import React from "react";
 import HomeHeader from "../components/HomeHeader/HomeHeader";
-import SiteSearch from "../components/SiteSearch/SiteSearch";
-import { MDBCol, MDBRow, MDBContainer, MDBIcon, MDBTooltip } from "mdbreact";
+import { MDBCol, MDBRow, MDBContainer, MDBView, MDBMask, MDBCardBody  } from "mdbreact";
+import code from "./images/code.jpg";
+import studying from "./images/studying.jpg";
+import meeting from "./images/meeting.jpg";
+import woman from "./images/woman-computer.jpg";
 
 function Landing() {
     return (
         <>
         <MDBContainer fluid>
             <HomeHeader/>
-            <MDBRow>
-                <MDBCol md="2"></MDBCol>
-                <MDBCol md="8">
-                    <h3>Our Goal</h3>
+
+            <section className="text-justify my-5">
+                <div className=" mx-auto mission-desc">
+                <MDBRow>
+                    <MDBCol md="3"></MDBCol>
+                    <MDBCol md="5">
+                        <h2 className=" mission-head font-weight-bold">
+                        Our Mission
+                        </h2>
+                    </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                    <MDBCol md="1"></MDBCol>
+                    <MDBCol md="10" >
+                        <h5 className="grey-text mx-auto mb-3">
+                        To ensure web applications enable everyone to use them effectively and without frustration, web accessibility standards must be considered. That's where we come in! Our learning site provides resources and learning for developers to ensure their sites can be used by all.
+                        </h5>
+                        <h5 className="grey-text mx-auto">
+                        As you work through our learning portal, join the conversation to ask questions, provide ideas and add answers for other developers working to improve their web accessibility standards. We look forward to engaging with you!
+                        </h5>
+                    </MDBCol>
+                </MDBRow>
+                </div>
+                <MDBRow className="d-flex justify-content-center card-row">
+                <MDBCol md="6" xl="5" className="mb-4">
+                    <MDBView className="overlay rounded z-depth-2" waves>
+                    <img src={code}
+                        alt="code image"
+                        className="img-fluid principles"
+                    />
+                    <a href="#!">
+                        <MDBMask overlay="white-slight" />
+                    </a>
+                    </MDBView>
+                    <MDBCardBody className="pb-0">
+                    <h4 className="font-weight-bold mb-3">Understanding the Four Principles of Accessibility</h4>
+                    <p>
+                        Our learning portal was built using the Web Content Accessibility Guidelines 2.0 and is structured using the four principles that lay the foundation for web use and content. To make the best use of our portal, understand what each principle considers.
+                    </p>
+                    <button className="principles-btn btn">
+                        Learn More
+                    </button>
+                    </MDBCardBody>
                 </MDBCol>
-            </MDBRow>
-            <MDBRow>
-                <MDBCol md="2"></MDBCol>
-                <MDBCol md="8">
-                    <p className="justify-content-start px-4 py-1">To ensure web applications enable everyone to use them effectively and without frustration, web accessibility standards must be considered. That's where we come in! Our learning site provides resources and learning for developers to ensure their sites can be used by all. </p>
-                    <p className="justify-content-start px-4 py-1">As you work through our learning portal, join the conversation to ask questions, provide ideas and add answers for other developers working to improve their web accessibility standards. We look forward to engaging with you!</p>
+                <MDBCol md="6" xl="5" className="mb-4">
+                    <MDBView className="overlay rounded z-depth-2" waves>
+                    <img
+                        src={woman}
+                        alt="woman on computer"
+                        className="img-fluid"
+                    />
+                    <a href="#!">
+                        <MDBMask overlay="white-slight" />
+                    </a>
+                    </MDBView>
+                    <MDBCardBody className="pb-0">
+                    <h4 className="font-weight-bold mb-3">Learn About Web Accessibility</h4>
+                    <p>
+                        Utilize our flashcards to learn more about each of the four principles and how you can improve your web content.
+                    </p>
+                    <a href="/learn">
+                        <button className="flashcards-btn btn">
+                            View Flashcards
+                        </button>
+                    </a>
+                    </MDBCardBody>
                 </MDBCol>
-            </MDBRow>
-            <MDBRow>
-                <MDBCol md="2"></MDBCol>
-                <MDBCol className=" ml-2 pl-4" md="3">
-                    <MDBTooltip 
-                            placement="bottom"
-                            component="MDBCol"
-                            tag="div"
-                            tooltipContent="Join the Conversation">
-                            <a href="/community" className="pl-1 mr-5">
-                                <MDBIcon icon="comments" size="4x" className="grey-text pr-3" alt="join the conversation" />
-                            </a>                    
-                        </MDBTooltip> 
+                <MDBCol md="6" xl="5" className="mb-4">
+                    <MDBView className="overlay rounded z-depth-2" waves>
+                    <img
+                        src={studying}
+                        alt="woman studying"
+                        className="img-fluid"
+                    />
+                    <a href="#!">
+                        <MDBMask overlay="white-slight" />
+                    </a>
+                    </MDBView>
+                    <MDBCardBody className="pb-0">
+                    <h4 className="font-weight-bold mb-3">Test Your Knowledge</h4>
+                    <p>
+                        After you've reviewed the flashcards, test your knowledge with our quizzes. There are questions for each of the four principles.
+                    </p>
+                    <a href="/quiz">
+                        <button className="quiz-btn btn">
+                            Take a Quiz
+                        </button>
+                    </a>
+
+                    </MDBCardBody>
                 </MDBCol>
-                <MDBCol md="3">
-                    <MDBTooltip 
-                        placement="bottom"
-                        component="MDBCol"
-                        tag="div"
-                        tooltipContent="Learn Web Accessibility">
-                        <a href="/learn" className="mr-5">
-                            <MDBIcon icon="puzzle-piece" size="4x" className="grey-text pr-3" alt="learn" />
-                        </a>                  
-                    </MDBTooltip> 
+                <MDBCol md="6" xl="5" className="mb-4">
+                    <MDBView className="overlay rounded z-depth-2" waves>
+                    <img
+                        src={meeting}
+                        alt="code discussion"
+                        className="img-fluid"
+                    />
+                    <a href="#!">
+                        <MDBMask overlay="white-slight" />
+                    </a>
+                    </MDBView>
+                    <MDBCardBody className="pb-0">
+
+                    <h4 className="font-weight-bold mb-3">Join the Conversation</h4>
+                    <p>
+                        Engage with other developers who are learning to make their sites more accessible. Join the community by adding comments and answering questions from others.
+                    </p>
+                    <a href="/community">
+                        <button className="conversation-btn btn">
+                            Learn More
+                        </button>
+                    </a>
+                    </MDBCardBody>
                 </MDBCol>
-                <MDBCol md="3">
-                    <MDBTooltip 
-                        placement="bottom"
-                        component="MDBCol"
-                        tag="div"
-                        tooltipContent="Take a Quiz">
-                        <a href="/quiz" className="mr-4">
-                            <MDBIcon icon="question" size="4x" className="grey-text pr-3" alt="take a quiz" />
-                        </a>                 
-                    </MDBTooltip> 
-                </MDBCol>
-            </MDBRow>
+                </MDBRow>
+            </section>
 
         </MDBContainer>
         </>
--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -14,11 +14,11 @@ function Resources() {
             </Header>
             
             <section className="my-5">
-            <MDBRow className="cardRow">
+            <MDBRow className="cardRow justify-content-center">
                 <MDBCol md="3" className="resourceSection1">
                 <MDBRow>
                     <MDBCol lg="2" md="3" size="2">
-                    <MDBIcon icon="universal-access" size="2x" className="cyan-text" />
+                    <MDBIcon icon="universal-access" size="2x" className="univ-icon" />
                     </MDBCol>
                     <MDBCol lg="10" md="9" size="10">
                     <h4 className="font-weight-bold">WebAIM</h4>
@@ -26,16 +26,16 @@ function Resources() {
                         Web Accessibility In Mind is an organization providing developers with 
                         information and suggestions to make web content more accessible to people with disabilities.
                     </p>
-                    <MDBBtn href="https://webaim.org" target="_blank" color="cyan darken-3" size="md">
+                    <button href="https://webaim.org" target="_blank" className="univ-button btn">
                         Learn more
-                    </MDBBtn>
+                    </button>
                     </MDBCol>
                 </MDBRow>
                 </MDBCol>
                 <MDBCol md="3" className="resourceSection2">
                 <MDBRow>
                     <MDBCol lg="2" md="3" size="2">
-                    <MDBIcon icon="video" size="2x" className="teal-text" />
+                    <MDBIcon icon="video" size="2x" className="video-icon" />
                     </MDBCol>
                     <MDBCol lg="10" md="9" size="10">
                     <h4 className="font-weight-bold">A11ycasts with Rob Dodson</h4>
@@ -43,16 +43,16 @@ function Resources() {
                         Learn how to build accessible apps through video with A11ycasts. 
                         This video series teaches the fundamentals of web accessibility using real world examples.
                     </p>
-                    <MDBBtn href="https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g" target="_blank" color="teal darken-3" size="md">
+                    <button href="https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g" target="_blank" className="video-button btn">
                         Learn more
-                    </MDBBtn>
+                    </button>
                     </MDBCol>
                 </MDBRow>
                 </MDBCol>
                 <MDBCol md="3" className="resourceSection3">
                 <MDBRow>
                     <MDBCol lg="2" md="3" size="2">
-                    <MDBIcon icon="cogs" size="2x" className="mdb-color-text" />
+                    <MDBIcon icon="cogs" size="2x" className="cogs-icon" />
                     </MDBCol>
                     <MDBCol lg="10" md="9" size="10">
                     <h4 className="font-weight-bold">Evaluation Tools</h4>
@@ -60,9 +60,9 @@ function Resources() {
                         An evaluation tool can review a website or software to determine how well it meets accessibility guidelines.
                         Review W3.org's list of available tools.
                     </p>
-                    <MDBBtn href="https://www.w3.org/WAI/ER/tools/" target="_blank" color="mdb-color" size="md">
+                    <button href="https://www.w3.org/WAI/ER/tools/" target="_blank" className="cogs-button btn">
                         Learn more
-                    </MDBBtn>
+                    </button>
                     </MDBCol>
                 </MDBRow>
                 </MDBCol>
