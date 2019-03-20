d6e774fe050df007be0d77de6eb7d7cf4cd80781
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-03-20 14:27:13 -0600

    updating resources page to fix issues

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -11,58 +11,46 @@ function Resources() {
                 <hr className="my-2" />
                 <h2 className="lead">Looking for more? Here are a few helpful resources to continue building enabled web applications.</h2>
             </Header>
-           <section>
+           <section className="text-center my-5">
            <MDBRow className="cardRow justify-content-center">
                 <MDBCol md="3" className="resourceSection1">
-                <MDBRow>
-                    <MDBCol className="justify-content-left" lg="2" md="3" size="2">
-                    <MDBIcon icon="universal-access" size="2x" className="univ-icon" />
-                    </MDBCol>
-                    <MDBCol lg="10" md="9" size="10">
-                    <h4 className="font-weight-bold">WebAIM</h4>
-                    <p className="grey-text">
-                        Web Accessibility In Mind is an organization providing developers with 
-                        information and suggestions to make web content more accessible to people with disabilities.
+                    <MDBIcon icon="universal-access" size="3x" className="univ-icon" />
+                    <h5 className="font-weight-bold my-4">WebAIM</h5>
+                    <p className="grey-text mb-md-0 mb-5">
+                    Web Accessibility In Mind is an organization providing developers with 
+                    information and suggestions to make web content more accessible to people with disabilities.
                     </p>
-                    <button href="https://webaim.org" target="_blank" className="univ-button btn">
-                        Learn more
-                    </button>
-                    </MDBCol>
-                </MDBRow>
+                    <a href="https://webaim.org" target="_blank">
+                        <button className="univ-button btn">
+                            Learn more
+                        </button>
+                    </a>
                 </MDBCol>
                 <MDBCol md="3" className="resourceSection2">
-                <MDBRow>
-                    <MDBCol lg="2" md="3" size="2">
-                    <MDBIcon icon="video" size="2x" className="video-icon" />
-                    </MDBCol>
-                    <MDBCol lg="10" md="9" size="10">
-                    <h4 className="font-weight-bold">A11ycasts with Rob Dodson</h4>
-                    <p className="grey-text">
-                        Learn how to build accessible apps through video with A11ycasts. 
-                        This video series teaches the fundamentals of web accessibility using real world examples.
+                    <MDBIcon icon="video" size="3x" className="video-icon" />
+                    <h5 className="font-weight-bold my-4">A11ycasts with Rob Dodson</h5>
+                    <p className="grey-text mb-md-0 mb-5">
+                    Learn how to build accessible apps through video with A11ycasts. 
+                    This video series teaches the fundamentals of web accessibility using real world examples.
                     </p>
-                    <button href="https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g" target="_blank" className="video-button btn">
-                        Learn more
-                    </button>
-                    </MDBCol>
-                </MDBRow>
+                    <a href="https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g" target="_blank">
+                        <button className="video-button btn">
+                            Learn more
+                        </button>
+                    </a>
                 </MDBCol>
                 <MDBCol md="3" className="resourceSection3">
-                <MDBRow>
-                    <MDBCol lg="2" md="3" size="2">
-                    <MDBIcon icon="cogs" size="2x" className="cogs-icon" />
-                    </MDBCol>
-                    <MDBCol lg="10" md="9" size="10">
-                    <h4 className="font-weight-bold">Evaluation Tools</h4>
-                    <p className="grey-text">
-                        An evaluation tool can review a website or software to determine how well it meets accessibility guidelines.
-                        Review W3.org's list of available tools.
+                    <MDBIcon icon="cogs" size="3x" className="cogs-icon" />
+                    <h5 className="font-weight-bold my-4">Evaluation Tools</h5>
+                    <p className="grey-text mb-md-0 mb-5">
+                    An evaluation tool can review a website or software to determine how well it meets accessibility guidelines.
+                    Review W3.org's list of available tools.
                     </p>
-                    <button href="https://www.w3.org/WAI/ER/tools/" target="_blank" className="cogs-button btn">
-                        Learn more
-                    </button>
-                    </MDBCol>
-                </MDBRow>
+                    <a href="https://www.w3.org/WAI/ER/tools/" target="_blank">
+                        <button className="cogs-button btn">
+                            Learn more
+                        </button>
+                    </a>
                 </MDBCol>
             </MDBRow>
             </section> 
@@ -74,3 +62,5 @@ function Resources() {
 };
 
 export default Resources;
+
+
